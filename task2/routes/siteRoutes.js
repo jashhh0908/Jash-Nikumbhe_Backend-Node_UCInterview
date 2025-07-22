06b037e61203e7166c259c9863acf03e654f6dd1
jash nikumbhe <jashunik9@gmail.com>
2025-07-22 12:08:52 +0530

    Update siteRoutes.js

--- task2/routes/siteRoutes.js
+++ task2/routes/siteRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
 
+const siteControllers = require('../controllers/siteControllers');
+
+router.get('/show-details', siteControllers.getShowDetails);
+router.get('/episodes', siteControllers.getEpisodes);
+
+module.exports = router;
